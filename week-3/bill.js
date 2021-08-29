0706e5ed5f5804e2234534c3803241d8dcc0bce6
Adam School <adrodgers@my365.bellevue.edu>
2021-08-29 17:40:28 -0500

    Week 3 fixes

--- week-3/bill.js
+++ week-3/bill.js
@@ -1,61 +1,47 @@
-export class Bill
-{
-    constructor(_beverages, _appetizers, _mainCourses, _desserts)
-    {
-        this._beverages = [];
-        this._appetizers = [];
-        this._mainCourses = [];
-        this._desserts = [];
-    }
+export class Bill {
+  constructor(_beverages, _appetizers, _mainCourses, _desserts) {
+    this._beverages = [];
+    this._appetizers = [];
+    this._mainCourses = [];
+    this._desserts = [];
+  }
 
-    addBeverage(beverage)
-    {
-        this._beverages.push(beverage);
-    }
+  addBeverage(beverage) {
+    this._beverages.push(beverage);
+  }
 
-    addAppetizer(appetizer)
-    {
-        this._appetizers.push(appetizer);
-    }
+  addAppetizer(appetizer) {
+    this._appetizers.push(appetizer);
+  }
 
-    addMainCourse(mainCourse)
-    {
-        this._mainCourses.push(mainCourse);
-    }
+  addMainCourse(mainCourse) {
+    this._mainCourses.push(mainCourse);
+  }
 
-    addDessert(dessert)
-    {
-        this._beverages.push(beverage);
-    }
+  addDessert(dessert) {
+    this._desserts.push(dessert);
+  }
 
-    getTotal() 
-    {
-    
-        let total = 0
-    
-        let beverageTotal = this._beverages.forEach(function(beverage)
-        {
-            total += parseFloat(beverage.price)
-        })
-    
-        let appetizerTotal = this._appetizers.forEach(function(appetizer)
-        {
-            total += parseFloat(appetizer.price)
-        })
-    
-        let mainCourseTotal = this._mainCourses.forEach(function(mainCourse)
-        {
-            total += parseFloat(mainCourse.price)
-        })
-    
-        let dessertTotal = this._desserts.forEach(function(dessert)
-        {    
-            total += parseFloat(dessert.price)
-        })
-    
-        let orderTotal = beverageTotal + appetizerTotal + mainCourseTotal + dessertTotal;
-        return orderTotal;
-    
-    }
+  getTotal() {
+    let total = 0;
 
+    let beverageTotal = this._beverages.forEach(function (beverage) {
+      total += parseFloat(beverage.price);
+    });
+
+    let appetizerTotal = this._appetizers.forEach(function (appetizer) {
+      total += parseFloat(appetizer.price);
+    });
+
+    let mainCourseTotal = this._mainCourses.forEach(function (mainCourse) {
+      total += parseFloat(mainCourse.price);
+    });
+
+    let dessertTotal = this._desserts.forEach(function (dessert) {
+      total += parseFloat(dessert.price);
+    });
+
+    let orderTotal = beverageTotal + appetizerTotal + mainCourseTotal + dessertTotal;
+    return orderTotal;
+  }
 }
