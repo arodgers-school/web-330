1d3c7e075e8ab172396924f6b0fd64e87bba4aae
Adam School <adrodgers@my365.bellevue.edu>
2021-09-12 17:03:27 -0500

    Assignment 5.2

---
+++ week-5/shopping-cart.js
@@ -0,0 +1,19 @@
+export class ShoppingCart {
+  constructor() {
+    this._products = [];
+  }
+
+  count() {
+    return this._products.length;
+  }
+
+  addProduct(product) {
+    this._products.push(product);
+  }
+
+  *[Symbol.iterator]() {
+    for (let product of this._products) {
+      yield product;
+    }
+  }
+}
